0a1821ccc838beeb2d5524a7367684e5d4614c8b
Jessie Edmisten <jaedmisten@gmail.com>
2021-07-26 20:04:29 -0400

    Added form validation to add weight form.

--- public/addWeightForm.php
+++ public/addWeightForm.php
@@ -4,7 +4,7 @@
     <div class="col-md-6 offset-md-3">
         <form action="insertWeight.php" method="post">
             <div class="row mb-3">
-                <label for="datetimepicker" class="col-sm-3 col-form-label">Date and Time:</label>
+                <label for="datetimepicker" class="col-sm-8 col-form-label">Date and Time (YYYY/MM/DD HH:MM):</label>
                 <div class="col-sm-10">
                     <input type="text" id="datetimepicker" class="form-control" name="dateTime" value="">
                 </div>
@@ -23,5 +23,38 @@
         </form>
     </div>
 </div>
+
+<script>
+$(document).ready(function() {
+    $('#submit').prop('disabled', true);
+
+    $('#weight').on('input', function() {
+        let weight = this.value;
+        let date = $('#datetimepicker').val()
+        verifyInput(date, weight);
+    });
+
+    $('#datetimepicker').on('input', function() {
+        let date = this.value;
+        let weight = $('#weight').val();
+        verifyInput(date, weight);
+    });
+
+    $('#datetimepicker').on('change', function() {
+        let date = this.value;
+        let weight = $('#weight').val();
+        verifyInput(date, weight);
+    });
+
+    function verifyInput(date, weight) {
+        console.log('verifyInput called');
+        if (!isNaN(weight) && weight != "" && new Date(date) != "Invalid Date") {
+            $('#submit').prop('disabled', false);
+        } else {
+            $('#submit').prop('disabled', true);
+        }
+    }
+});
+</script>
 
 <?php include('footer.php') ?>
--- public/insertWeight.php
+++ public/insertWeight.php
@@ -13,32 +13,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     print_r($_POST);
     echo '</pre>';
 
-    if (isset($_POST["dateTime"])) {
+    if (isset($_POST["dateTime"]) && $_POST["dateTime"] != "") {
         $dateTime = $_POST["dateTime"];
+        echo "<p>dateTime: $dateTime</p>";
     }
     if (isset($_POST["weight"]) && is_numeric($_POST["weight"])) {
         $weight = $_POST["weight"];
+        echo "<p>weight: $weight</p>";
     }
+    if (isset($dateTime) && isset($weight)) {
+        try {
+            $sql = 'INSERT INTO weights (`weight`, `date`) VALUES (?, ?)';
+            $stmt = $pdo->prepare($sql);
+            $result = $stmt->execute([$weight, $dateTime]);
+            echo '<pre>';
+            print_r($result);
+            echo '</pre>';
 
-    try {
-        $sql = 'INSERT INTO weights (`weight`, `date`) VALUES (?, ?)';
-        $stmt = $pdo->prepare($sql);
-        $result = $stmt->execute([$weight, $dateTime]);
-        echo '<pre>';
-        print_r($result);
-        echo '</pre>';
-
-        if ($result) {
-            header('Location: viewWeights.php');
-        } else {
-            echo "THERE WAS AN ERROR INSERTING WEIGHT";
+            if ($result) {
+                header('Location: viewWeights.php');
+            } else {
+                echo "THERE WAS AN ERROR INSERTING WEIGHT";
+            }
+        } catch (PDOException $e) {
+            echo '<pre>';
+            print_r($e);
+            echo '</pre>';
+            echo $e->getMessage();
         }
-    } catch (PDOException $e) {
-        echo '<pre>';
-        print_r($e);
-        echo '</pre>';
-        echo $e->getMessage();
+    } else {
+        echo "incorrect data to insert. Will need to redirect to form with error.";
     }
-
 }
 
