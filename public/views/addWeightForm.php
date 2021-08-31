d36f7ab1b02c68ba377bc7df99f5df62573f4348
Jessie Edmisten <jaedmisten@gmail.com>
2021-08-30 20:12:00 -0400

    Put content on add weights page in separate bootstrap rows and columns. Centered form.

--- public/views/addWeightForm.php
+++ public/views/addWeightForm.php
@@ -2,22 +2,27 @@
 <div class="row">
     <div class="col-md-6 offset-md-3">
         <h2 class="page-header">Add Weight Record</h2>
+    </div>
+</div>
+<div>
+<div class="row">
+    <div class="col-md-4 offset-md-4">
         <div class="add-weight-error-msg"></div>
         <form>
             <div class="row mb-3">
-                <label for="datetimepicker" class="col-sm-8 col-form-label">Date and Time (YYYY/MM/DD HH:MM)</label>
-                <div class="col-sm-10">
+                <label for="datetimepicker" class="col-sm-8 offset-sm-2 col-form-label">Date and Time (YYYY/MM/DD HH:MM)</label>
+                <div class="col-sm-8 offset-sm-2">
                     <input type="text" id="datetimepicker" class="form-control" name="dateTime" value="">
                 </div>
             </div>
             <div class="row mb-3">
-                <label for="weight" class="col-sm-3 col-form-label">Weight (LBS)</label>
-                <div class="col-sm-10">
+                <label for="weight" class="col-sm-5 offset-sm-2 col-form-label">Weight (LBS)</label>
+                <div class="col-sm-8 offset-sm-2">
                     <input type="number" id="weight" step=".1" class="form-control" name="weight" value="">
                 </div>
             </div>
             <div class="row">
-                <div class="col-auto">
+                <div class="col-auto offset-sm-2">
                     <input type="submit" id="submit" class="btn btn-primary" name="submit">
                 </div>
             </div>
